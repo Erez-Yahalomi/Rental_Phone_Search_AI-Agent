e7e9bac99511bdf209c46edfc4b388615d403131
ארז  יהלומי <229332915+Erez-Yahalomi@users.noreply.github.com>
2025-11-09 09:41:22 +0200

    Delete apps_ingestion_base_provider.py

--- apps_ingestion_base_provider.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Iterable
-from .filters import RentalFilters
-from .models import Listing
-
-class ListingProvider(ABC):
-    @abstractmethod
-    def search(self, filters: RentalFilters, limit: int) -> Iterable[Listing]:
-        raise NotImplementedError
